84c2fbb6c3876fd98325b52779a4a21f863854a2
Jay <Axon284053@gmail.com>
2023-06-16 09:38:31 -0700

    Work in progress for turn based and fight with options

---
+++ classmaking.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+class Character {
+    private:
+        string name;
+        int age;
+        int currenthealth;
+        int maxhealth;
+        int strength;
+        int dexterity;
+        int defense;
+    public:
+        Character(const string& characterName, int characterAge, int characterCurrentHP, int characterMaxHP, int characterStrength, int characterDexterity, int characterDefense) {
+            name = characterName;
+            age = characterAge;
+            currenthealth = characterCurrentHP;
+            maxhealth = characterMaxHP;
+            strength = characterStrength;
+            dexterity = characterDexterity;
+            defense = characterDefense;   
+        }
+
+        void attack(Character& attacker, Character& defender) {
+            int damage = attacker.strength - defender.defense;
+            if (damage > 0) {
+                defender.currenthealth -= damage;
+            }
+            cout << name << "Swings their sword at the beast.\n\n";
+            cout << defender.name << " has " << defender.currenthealth << "/" << defender.maxhealth << "HP after that hit. \n\n";
+        }
+};
+
+int main() {
+    Character player("Player", 20, 100, 100, 25, 10, 20);
+    Character enemy("Enemy", 50, 75, 75, 25, 10, 10);
+
+    bool isPlayerTurn = true;
+    bool isBattleOver = false;
+
+    while (!isBattleOver) {
+        if (isPlayerTurn) {
+            cout << "It is currently Player's turn, take an action: \n";
+            cout << "1. Attack\n";
+            cout << "2. Defend\n";
+            cout << "3. Run away\n";
+            int choice;
+            cin >> choice;
+
+        switch (choice) {
+            case 1:
+                player.attack(enemy);
+                break;
+            case 2:
+                cout << "Player takes a defensive stance. \n";
+                break;
+            case 3:
+                cout << "Player runs away from the fight. \n";
+                isBattleOver = true;
+                break;
+            default:
+                cout << "invalid choice. Turn skipped. \n";
+            }
+        } else {
+            cout << "Enemy's turn. \n";
+            enemy.attack(enemy, player);
+        }
+
+        if (player.currenthealth <= 0 || enemy.currenthealth <= 0) {
+            isBattleOver = true;
+            if (player.currenthealth <= 0)
+                cout << "Player has been defeated. Game Over! \n";
+
+                else
+                cout << "Enemy has been defeated. You Win! \n";
+        }
+
+        isPlayerTurn = !isPlayerTurn;
+    }
+
+    return 0;
+
+}
