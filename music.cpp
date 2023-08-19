589662d23874e471719580bdf33588852941f2f9
Jay <Axon284053@gmail.com>
2023-08-18 19:41:31 -0700

    more ideas ahead

--- music.cpp
+++ music.cpp
@@ -2,3 +2,6 @@
 // get an mp3 file or mp4 file to play while in menu\]
 
 // import pygame pygame.init()
+
+
+// you need to make a list of keywords to add to the library keywords so that you can easily add them to the program so you can start building an encyclopedia
