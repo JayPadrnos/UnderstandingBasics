311da7c8f64f679c925eb69dcaabea17ef32447d
Jay <Axon284053@gmail.com>
2023-10-15 15:50:04 -0700

    need to get back to it somethin beter

--- bordum.cpp
+++ bordum.cpp
@@ -1,1 +1,15 @@
 // Get an SFML
+
+//I need a 2D visual space
+//user needs to be able to move and interact with other objects in the room
+//there needs to be multiple rooms
+//there needs to be doors in between certain rooms where keys are needed 
+//there needs to be items for the player and inventory
+
+
+//needs enemies 
+//needs boss type 
+//enemies need inventory with % to drop items they equipped 
+//stats and skills menu
+//different attacks for weapons
+//skills for player to have moves that are not dependent on weapons but do different things with dif weapons
