1298a480db6b1a6d3d0deecf593d5afeef88d67d
Jay <Axon284053@gmail.com>
2023-10-16 23:31:17 -0700

    oh shh

--- bordum.cpp
+++ bordum.cpp
@@ -14,3 +14,8 @@
 //different attacks for weapons
 //skills for player to have moves that are not dependent on weapons but do different things with dif weapons
 // different classes of characters or rather skill paths they can go down
+// cosmetic for character to change color shirt pants hair and eyes
+// name character and have save files for the characters
+// have an mp3 playing for backround music and an adjust volume meter
+// have animation for character using key items or levers or buttons
+// have an end screen animation then credits when beating the final boss with a continue play and respawn player at spawn and reset the world while increasing difficulty
