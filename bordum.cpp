0c9d38902680cd2837b7bae1615732644d06662f
Jay <Axon284053@gmail.com>
2023-10-18 19:15:15 -0700

    hmm

--- bordum.cpp
+++ bordum.cpp
@@ -19,3 +19,40 @@
 // have an mp3 playing for backround music and an adjust volume meter
 // have animation for character using key items or levers or buttons
 // have an end screen animation then credits when beating the final boss with a continue play and respawn player at spawn and reset the world while increasing difficulty
+
+
+
+// DirectX for API allow for Proton through steam allowing for Linux to run the game.
+// Get an SMFL like MonoGame which is in C# and Love2D which is in Lua Roblox type these are both for 2D
+// Efficient rendering collision detection and game logic optimize critical parts of engine
+// SDL_mixer or OpenAL for Audio
+// Design input handing for keyboard mouse and controller
+// Debugging tools
+// Cross platform libraries
+
+
+// Develop Graphics Engine that can render 2D or 3D 
+// Optimize
+
+// Physics Engine handle collision detection and phyusics simulation especially if complex physics interactions
+
+// If world is proceedurally generated algorithms and systems for world gen (terrain biomes etc)
+
+// Game Logic rules logic mechanisms character behavior item systems and progression
+
+// Audio Engine handles music SFX and spatial audio
+
+// Networking for multiplayer and local play along with a LAN system
+
+// Test test test debug
+
+// Optimize performance memory usage and responsiveness
+
+// Document and engage in community along with if published prepare for distribution on various platforms
+
+
+
+
+
+///// Visualizer with audio
+////// Push the button Game
