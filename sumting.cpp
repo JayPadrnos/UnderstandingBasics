19ba08d37a5804948008fd935d25320de58fb201
Jay <Axon284053@gmail.com>
2023-12-05 05:04:55 -0800

    yup

--- sumting.cpp
+++ sumting.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int num1;
 int num2;
+int choice;
 
 int main() {
     cout << "choose a number" << endl;
@@ -14,7 +15,13 @@ int main() {
     cout << "choose another number" << endl;
     cin >> num2;
 
-    if 
-    num1 > num2;
+    cout << "what operater would you like to choose";
+    cin >> choice;
+
+    switch (choice) {
+        case '+' :
+            cout << num1 << choice << num2 << " = " << answer << "\n";
+            break;
     // make display to state which number is bigger than the other
+    }
 }
