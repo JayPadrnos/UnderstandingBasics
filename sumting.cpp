675c5b4c22a88f2922772a83c75ccb10ed9ad5cb
Jay <Axon284053@gmail.com>
2023-12-04 20:20:55 -0800

    huh

---
+++ sumting.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int num1;
+int num2;
+
+int main() {
+    cout << "choose a number" << endl;
+    cin >> num1;
+
+    cout << "choose another number" << endl;
+    cin >> num2;
+
+    if 
+    num1 > num2;
+    // make display to state which number is bigger than the other
+}
