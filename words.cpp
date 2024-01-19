9b68a48474b3e8f6eb9c44b11cea85dcc702591b
Jay <Axon284053@gmail.com>
2024-01-19 12:40:37 -0800

    words

--- words.cpp
+++ words.cpp
@@ -1,11 +1,22 @@
 /*
-perpendicular
-trapizod
-homunculus
-indubidably
-studious
-confluence
+susurrus = "soo sur uhs" = noun = a soft murmuring or rustling sound; whisper.
+
+perpendicular = "per puhn dik yuh ler" = adjective = vertical straight up and down; upright = noun = instrument for indicating the vertical line from any point.
+
+trapezoid = "trap u zoid" = adjective = Also trap e zoidal Geometry of relating to or having the form of a trapezoid = noun = a quadrilateral place figure having two parallel and two non parallel sides. 
+
+homunculus = "huh muhng kyuh luhs" = noun = A fully formed, miniature human body believed, according to some medical  theories of 16th and 17th centuries, to be contained in the spermatozoon. = noun2 = a diminutive human being.
+
+indubitably = "in doo bi tuh blee" = adverb in a way that is paternilly evident or certain unquestionably. without doubt
+
+studious = "stoo dee uhs" = adjective = disposed or gien diligent study = noun2 = concerned with, characterized by or pertaining to study.
+
+confluence = "kon floo uhns" = noun = a flowing together of two or more streams, rivers, or the like. = noun2 = their place of junction.
+
 equinamity
-equilibrium
+
+equilibrium = "ee kwuh lib ree uhm" = noun a state of rest or balance due to the equal action of opposing forces = noun2 = equal balance between any powers, influences, etc; equality of effect
+
 copius
+
 flabergasted*/
