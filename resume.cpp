1fa5cc5d5f30c621076829c33e33084d773f5448
Jay <Axon284053@gmail.com>
2024-01-22 12:21:14 -0800

    OOP

---
+++ resume.cpp
@@ -0,0 +1,49 @@
+/*
+RESUME
+Summary // third person
+
+
+Work Experience //
+
+
+
+Skills //
+Proccess Orientated
+
+
+
+make a linkden
+
+neetcode, learn git
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Interview questions
+
+with cout all the even numbers between 1 and 10 including both 1 and 10
+
+#include <iostream>
+
+int main() {
+
+}
+
+*/
