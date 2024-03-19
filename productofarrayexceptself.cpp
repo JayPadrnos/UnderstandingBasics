f8255e0ddf054e11bf5af443ba130fe4d93642da
Jay <Axon284053@gmail.com>
2024-03-18 17:15:33 -0700

    Product of array except self

---
+++ productofarrayexceptself.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+class Solution {
+    public:
+        std::vector<int> productExceptSelft(std::vector<int>& nums) {
+            int n = nums.size();
+            std::vector<int> answer(n, 1);
+
+            int left_product = 1;
+            for (int i = 0; i < n; ++i) {
+                answer[i] *= left_product;
+                left_product *= nums[i];
+            }
+
+            int right_product = 1;
+            for (int i = n - 1; i >= 0; --i) {
+                answer[i] *= right_product;
+                right_product *= nums[i];
+            }
+
+            return answer;
+        }
+};
