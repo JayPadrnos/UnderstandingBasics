d155c3b992490f72e439da5d3c5a02f5fd9db3b9
Jay <Axon284053@gmail.com>
2024-04-04 16:01:07 -0700

    water container

---
+++ LeetCodeProblems/Two Pointers/containerwithmostwater.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+    public:
+        int maxArea(std::vector<int>& height) {
+            int maxArea = 0;
+            int left = 0;
+            int right = height.size() - 1;
+
+            while (left < right) {
+                int minHeight = std::min(height[left], height[right]);
+                int width = right - left;
+                int area = minHeight * width;
+                maxArea = std::max(maxArea, area);
+
+                // Move the pointer pointing to the smaller height
+                if (height[left] < height[right]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+};
