43607af83129213a5b6b9c95fb382312db5cfab0
Jay <Axon284053@gmail.com>
2024-07-18 11:58:23 -0700

    max product subarray

---
+++ LeetCodeProblems/1D-DP/maxproductsubarray.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+        int result = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < 0) {
+                swap(maxProduct, minProduct);
+            }
+
+            maxProduct = max(nums[i], maxProduct * nums[i]);
+            minProduct = min(nums[i], minProduct * nums[i]);
+
+            result = max(result, maxProduct);
+        }
+
+        return result;
+    }
+};
