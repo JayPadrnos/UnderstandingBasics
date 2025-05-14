c101248e511b784ac2434f85c3c83beb901420e6
JayPadrnos <Axon284053@gmail.com>
2025-05-13 20:52:06 -0500

    it begins

---
+++ Exercise/Routine.cpp
@@ -0,0 +1,4 @@
+// Let's make a program to setup a workout routine that will be somewhat rng as to the workouts available
+// in turn allowing us to keep the workout fresh and continueing to work on the full body
+
+// Refer to chat for the real project starting and double down on it 5 hours  a day now less
