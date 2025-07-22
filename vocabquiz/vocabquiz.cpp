4cadfd83db4946e7379c06d6bedf6b0a4408e2d2
JayPadrnos <Axon284053@gmail.com>
2025-07-21 23:15:18 -0500

    hira kata flashcard?

--- vocabquiz/vocabquiz.cpp
+++ vocabquiz/vocabquiz.cpp
@@ -6,3 +6,6 @@ struct VocabItem {
 }
 
 // struct for japanese language needs work and other questionare for archetype
+// redo the flash card make it for the hiragana and katana symbols so that you can identify easier set
+// up something like duolingo??? so user can view all at once and choose which one to even hear if clicked on
+// set it up like a learning game?
