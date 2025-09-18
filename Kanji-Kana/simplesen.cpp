5d16eba153e268f33f69a3688d7bd8ea0e55458a
JayPadrnos <Axon284053@gmail.com>
2025-09-17 21:11:47 -0500

    more to come

---
+++ Kanji-Kana/simplesen.cpp
@@ -0,0 +1,7 @@
+// have kana no kanji based sentences that are proper and also go onto find more complex ways of phrasing 
+
+// set levels of sentences up to level 5 so as which to run diferent quiz types
+
+// be sure to have full impliment into the system as to have the words in sentences in library and such
+
+// 
