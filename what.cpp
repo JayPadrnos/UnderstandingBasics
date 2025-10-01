f1de70a1ba4a26384566edfa86ee54a0127db6fa
JayPadrnos <Axon284053@gmail.com>
2025-10-01 16:11:30 -0500

    mapping out the plan

---
+++ what.cpp
@@ -0,0 +1,5 @@
+// create new git repo for this project as it begins today October 1st, 2025 
+
+// figure out how to schedule a 5 day week on work and a 6th rest day that still exposes you to the content for project
+
+// 
